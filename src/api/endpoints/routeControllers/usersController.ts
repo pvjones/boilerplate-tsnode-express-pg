b2ae4beac000ad04d447118510d30b702ce8da9e
Paul Jones <paul.jones@observepoint.com>
2018-04-06 17:21:59 -0700

    test route working

--- src/api/endpoints/routeControllers/usersController.ts
+++ src/api/endpoints/routeControllers/usersController.ts
@@ -1,7 +1,9 @@
 import { Application } from 'express'
 
 const registerRoutes = (app: Application) => {
-
+  app.get('/api/user', (req, res, next) => {
+    res.send('Test User')
+  })
 }
 
 export default registerRoutes
