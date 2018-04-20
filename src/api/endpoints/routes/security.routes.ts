6ffbef69f662423497c3d42e31bb24e302711a5e
Paul Jones <paul.jones@observepoint.com>
2018-04-20 14:24:50 -0600

    added isAuthenticated to logout route

--- src/api/endpoints/routes/security.routes.ts
+++ src/api/endpoints/routes/security.routes.ts
@@ -1,12 +1,12 @@
 import { Application } from '../../models'
-import { handleRequest } from '../../utils/express.utils'
+import { handleRequest, isAuthenticated } from '../../utils/express.utils'
 import * as controllers from '../controllers/security.controller'
 
 const routes = (app: Application): void => {
 
   app.post('/api/auth/register', handleRequest(controllers.register))
   app.post('/api/auth/login', handleRequest(controllers.login))
-  app.post('/api/auth/logout', handleRequest(controllers.logout))
+  app.post('/api/auth/logout', isAuthenticated, handleRequest(controllers.logout))
 
 }
 
